480dc260b4efd455e43a0ea3d5f47c1981323acc
Dawid Ochman <dawid.ochman99@gmail.com>
2024-05-11 20:57:03 +0200

    dodałem cool tło

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "Menu.h"
 #include <QtWidgets/QApplication>
+#include <Windows.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Menu w;
+    w.setWindowState(Qt::WindowMaximized);
     w.show();
     return a.exec();
 }
